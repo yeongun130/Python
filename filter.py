c6525f2eac77e7b0a78f05dd6839c2f3d13cf12d
yeongun130 <yeongunj625@gmail.com>
2023-05-23 16:43:02 +0900

    Python_filtering 연습

--- filter.py
+++ filter.py
@@ -5,10 +5,13 @@ img = plt.imread('./test.jpg').mean(axis=-1)
 H, W = img.shape
 print(H, W)
 
-filter_ = np.array([[0, 255, 255],
-                    [0, 255, 255],
-                    [0, 255, 255]])
-H_filter, W_filter = filter_.shape
+filter_x = np.array([[-1, 0, 1],
+                    [-2, 0, 2],
+                    [-1, 0, 1]])
+filter_y = np.array([[1, 2, 1],
+                    [0, 0, 0],
+                    [-1, -2, -1]])
+H_filter, W_filter = filter_x.shape
 H_, W_ = H - H_filter + 1, W - W_filter + 1
 print(H_, W_)
 
@@ -16,11 +19,13 @@ img_filtered = np.zeros(shape=(H_, W_))
 for i in range(H_):
     for j in range(W_):
         window = img[i : i + 3, j : j + 3]
-        z = np.sum(window * filter_)
-        img_filtered[i, j] = np.abs(z)
+        z_x = np.sum(window * filter_x)
+        z_y = np.sum(window * filter_y)
+        img_filtered[i, j] = z_x**2 + z_y**2
 
 fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 axes[0].imshow(img, cmap='gray')
 axes[1].imshow(img_filtered, cmap='gray')
 fig.tight_layout()
+fig.savefig('tmp.png')
 plt.show()
